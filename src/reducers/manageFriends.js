79f582c00743b0edc36b507663b222529e109e90
zach6585 <zmines@wesleyan.edu>
2021-05-16 15:47:41 -0400

    commit tree 0320814d4524170f25da6d61b634be2be2ebf7d4

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch(action.type) {
+        case 'ADD_FRIEND':
+            return {...state, friends: state.friends.concat(action.friend)};
+        
+        case 'REMOVE_FRIEND':
+            return {...state, friends: state.friends.filter(element => element.id !== action.id)}
+
+        default:
+            return state;
+    }
 }
